602ed304f62098abfca2b3e7544b8822e5a43347
pbircher <bircher.peter@mirango.hu>
2020-12-15 22:06:15 +0100

    add port to env var

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const session = require("express-session");
-const port = 3000;
 require("dotenv").config();
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
 app.disable("etag");
+
+const port = process.env.PORT;
 
 app.use(
   session({
